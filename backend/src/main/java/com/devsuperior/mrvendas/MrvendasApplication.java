d3b5c1fa39e0419f173484c15e9493c90a8fd15e
marciomrcr <marcio.mrcr@outlook.com>
2021-11-02 12:01:59 -0300

    project created

---
+++ backend/src/main/java/com/devsuperior/mrvendas/MrvendasApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.mrvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/mrvendas/MrvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.mrvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
