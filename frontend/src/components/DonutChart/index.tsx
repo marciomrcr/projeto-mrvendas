a567c3d8134f0745e0e033c31d402711cdab577a
marciomrcr <marcio.mrcr@outlook.com>
2021-11-23 21:06:53 -0300

    BarChart Integração

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -25,11 +25,6 @@ const DonutChart = () => {
     });
   }, []);
 
-  /*const mockData = {
-    series: [477138, 499928, 444867, 220426],
-    labels: ["Lucena", "Log Comercial", "Transglobal", "RS Engenharia"],
-  }; */
-
   const options = {
     legend: {
       show: true,
